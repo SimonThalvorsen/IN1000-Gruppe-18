e4e311d0124aef43c0a70b33aa3518a074f2aeaf
Simon <simon.thalvorsen@gmail.com>
2024-08-27 13:32:07 +0200

    uke 01 time 01

---
+++ uke01/test.py
@@ -0,0 +1,26 @@
+x = 10
+navn = "Simon"
+a = 'a'
+x = 12
+y = x + 5
+
+
+# Tar inn input fra bruker, skriver det ut i terminalen
+# navn = input("Hei! Hva heter du? \n>")
+
+#Printer ut verdien i navn
+# print("Hei " + navn)
+x = 10
+if x == 12:
+    print("X har verdien 12")
+elif x == 10:
+    print("X har verdien 10")
+else:
+    print("X har IKKE verdien 12")
+
+
+
+
+
+
+
