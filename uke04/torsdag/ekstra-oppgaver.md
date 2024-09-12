7babaaababa70d7f52051b45a5d843867452b717
Simon <simon.thalvorsen@gmail.com>
2024-09-12 14:49:33 +0200

    livekode + kodefeil i ekstra.oppgaver

--- uke04/torsdag/ekstra-oppgaver.md
+++ uke04/torsdag/ekstra-oppgaver.md
@@ -20,8 +20,8 @@ a = 10
 b = 1
 
 while a > 0:
-b = b \* 2
-a = a - b
+  b = b * 2
+  a = a - b
 
 print("a =", a)
 print("b =", b)
---
+++ uke04/torsdag/livekode.py
@@ -0,0 +1,37 @@
+def while_lokke():
+    i = 0
+    while i < 10:
+        print(f"I har nå verdien {i}")
+        i += 1
+        
+def for_lokke():
+    for i in range(0, 11, 2):
+        print(f"I har nå verdien {i}")
+
+def for_each():
+    liste = ["Ski", "Løping", "Gåing", "Alpin"]
+    for element in liste:
+        print(element)
+
+    index = 0
+    while index < len(liste):
+        print(liste[index])
+        index += 1
+
+    
+def test1():
+    i = 0
+    while i > 10:
+        print(i)
+        i += 1
+        
+        
+def test2():
+    i = 0
+    while i < 10:
+        print(i)
+        
+        
+
+
+
