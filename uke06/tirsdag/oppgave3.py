e4a68dac302b1945b695a7cc25787d3721d70910
Simon <simon.thalvorsen@gmail.com>
2024-10-01 15:07:44 +0200

    la til originale koden 3

--- uke06/tirsdag/oppgave3.py
+++ uke06/tirsdag/oppgave3.py
@@ -11,4 +11,20 @@ def hovedprogram():
     fn_paul = "Paul.txt"
     print("Paul har brukt: ", kalkuler_utgifter(fn_paul))
 
+
+    # fn_peter = "Peter.txt"
+    # tot_peter = 0
+    #
+    # for line in open(fn_peter):
+    #     utgift_peter = int(line)
+    #     tot_peter += utgift_peter
+    #     print("Peter har brukt: ", tot_peter)
+    #
+    #
+    # fn_paul = "Paul.txt"
+    # tot_paul = 0
+    # for line in open(fn_paul):
+    #     utgift_paul = int(line)
+    #     tot_paul += utgift_paul
+    #     print("Paul har brukt: ", tot_paul)
 hovedprogram()
