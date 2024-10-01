ba14fe9088a59ee31bf77b6041561aa881cc5acc
Simon <simon.thalvorsen@gmail.com>
2024-10-01 15:10:52 +0200

    la til en liten kommentar for liste-genereringen

--- uke06/tirsdag/oppgave6.py
+++ uke06/tirsdag/oppgave6.py
@@ -14,6 +14,9 @@ def hovedprogram():
 
     s_idx = 1
     u_idx = 1
+    
+    # Prøve gjerne å forstå hva som skjer her, men det er ABSOLUTT ikke forventet kode dere skal kunne i IN1000
+    # Det den gjør er å generere en liste av lister, hvor ~50% er sorterte. Alle listene har variabel størrelse osv.
     talllister = [
     sorted([random.randint(1, 50) for _ in range(random.randint(5, 15))]) if random.choice([0, 1])
     else [random.randint(1, 50) for _ in range(random.randint(5, 15))]
