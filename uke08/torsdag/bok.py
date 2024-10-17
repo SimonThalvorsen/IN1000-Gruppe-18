868851b78f5184302dbd8fa2ca9ba5dc445ce8ca
SimonTHalvorsen <simon.thalvorsen@gmail.com>
2024-10-17 15:00:32 +0200

    eksempel lt/gt/str/eq

--- uke08/torsdag/bok.py
+++ uke08/torsdag/bok.py
@@ -5,9 +5,24 @@ class Bok:
         self._isbn = isbn
         self._er_tilgjenglig = True
 
+    def __str__(self):
+        return f"{self._tittel}:  {self._forfatter}"
+
+    def __repr__(self):
+        return f"{self._tittel}:  {self._forfatter}"
+
+    def __eq__(self, annen):
+        return (self._tittel == annen._tittel) and (self._forfatter == annen._forfatter)
+
+    def __gt__(self, annen):
+        return (self._tittel > annen._tittel) and (self._forfatter > annen._forfatter)
+
+    def __lt__(self, annen):
+        return (self._tittel < annen._tittel) and (self._forfatter < annen._forfatter)
+
     def er_tilgjengelig(self):
         return self._er_tilgjenglig
-    
+
     def laan_ut(self):
         self._er_tilgjenglig = False
 
@@ -16,12 +31,12 @@ class Bok:
 
     def hent_tittel(self):
         return self._tittel
-    
+
     def hent_forfatter(self):
         return self._forfatter
-    
+
     def hent_isbn(self):
         return self._isbn
-    
+
     def tittel_inneholder(self, str):
         return str.lower() in self._tittel.lower()
--- uke08/torsdag/livekode.py
+++ uke08/torsdag/livekode.py
@@ -1,19 +1,28 @@
 from bibliotek import Bibliotek
+from bok import Bok
+
 
 def main():
     bibliotek = Bibliotek()
     bibliotek.les_fra_fil("books.txt")
-    
+
     hobbit = bibliotek.finn_bok("The Hobbit")
     hobbit.laan_ut()
 
     boker = bibliotek.finn_boker("the")
+    print(boker)
+    print(sorted(boker))
+
+    bok1 = Bok("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 123456789)
+    bok2 = Bok("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 112233445)
+    print(bok1 == bok2)
+
     for bok in boker:
+        print(bok)
         if bok.er_tilgjengelig():
             print(bok.hent_tittel(), "er tilgjengelig for utlån.")
         else:
             print(bok.hent_tittel(), "er utlånt.")
-    
-    
+
 
 main()
